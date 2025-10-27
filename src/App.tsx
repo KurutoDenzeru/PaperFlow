0750b3f89ad9b5e5fc071ea5596dda335805c1cd
KurutoDenzeru <kurtdenzel51@gmail.com>
2025-10-27 05:15:37 -0700

    feat: add button component and integrate with Tailwind CSS; update dependencies and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -18,9 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
